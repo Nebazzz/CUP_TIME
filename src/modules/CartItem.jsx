0d76408cda9d47ae5253f8caba1d76355f175484
Nebazzz <johnpechkin@gmail.com>
2024-08-02 23:05:27 +0300

    style cart done

--- src/modules/CartItem.jsx
+++ src/modules/CartItem.jsx
@@ -5,9 +5,8 @@ export const CartItem = ({data}) => (
     <div className="cart-item__info">
       <h3 className="cart-item__title">{data.title}</h3>
       <div className="cart-item__quantity">
-        <button className="cart-item__quantity-button cart-item__quantity-button_minus">
+        <button className="cart-item__quantity-button cart-item__quantity-button_minus"></button>
         <input className="cart-item__quantity-input" type="number" value={1}></input>
-        </button>
         <button className="cart-item__quantity-button cart-item__quantity-button_plus"></button>
       </div>
       <p className="cart-item__price">{data.price}&nbsp;₽</p>
